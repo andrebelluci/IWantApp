5b73c754ffc03ec340ce721d7f7f422c924abaa4
Andre Belluci <andre.belluci@sitemercado.com.br>
2022-12-27 15:17:49 -0300

    add flunt validations

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,7 +1,20 @@
-﻿namespace IWantApp.Domain.Products;
+﻿using Flunt.Validations;
+using System.Diagnostics.Contracts;
+
+namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
     public string Name { get; set; }
-    public bool Active { get; set; } = true;
+    public bool Active { get; set; }
+
+    public Category(string name)
+    {
+        var contract = new Contract<Category>()
+            .IsNotNullOrEmpty(name, "Name", "Name is required");
+        AddNotifications(contract);
+
+        Name = name;
+        Active = true;
+    }
 }
